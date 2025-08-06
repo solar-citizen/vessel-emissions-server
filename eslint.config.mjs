f7a2afbbbe44c1927c41d59b4cb17f7a42da3dca
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-08-06 16:25:51 +0300

    Add/update configs/docs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import esLintImport from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
@@ -17,18 +20,23 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
+      parser: tsParser,
       parserOptions: {
-        projectService: true,
+        project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+    plugins: {
+      import: esLintImport,
+      'simple-import-sort': simpleImportSort,
     },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
